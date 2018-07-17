dcd326e3a757391701c612da6faade0e12c0e853
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-07-17 22:19:26 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,7 +11,8 @@ class School
  
  def add_student(student_name, grade)
   roster[grade] = []
-  roster[grade] << student_name ||= student_name << student_name
+  roster[grade] << student_name 
+  # ||= student_name << student_name
 end
  
 
