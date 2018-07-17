e794bfec07d1661bd2416c2a0776a60b00a77fcc
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-07-17 22:09:41 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
 # code here!
+
+class School
+  attr_accessor :name, :roster 
+  
+  
+   def initialize(name)
+   @name = name
+   @roster = {}
+ end
+ 
+ def add_student(student_name, grade)
+  roster[grade] = []
+  roster[grade] << student_name ||= student_name << student_name
+end
+ 
+
+ 
+end
+
+
+  # school = School.new("Bayside High School")
